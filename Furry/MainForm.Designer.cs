a72735f77c7b93ebbe88ac31533c1c7f3c563339
Андрей Коленков <avoknelok@mail.ru>
2021-05-04 21:48:35 +0300

    Косметическое нововведение

--- Furry/MainForm.Designer.cs
+++ Furry/MainForm.Designer.cs
@@ -29,9 +29,9 @@ namespace Furry
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.inputButton = new System.Windows.Forms.Button();
@@ -68,7 +68,7 @@ namespace Furry
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 461);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -89,15 +89,15 @@ namespace Furry
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(314, 444);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(347, 455);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // inputButton
             // 
             this.inputButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inputButton.Location = new System.Drawing.Point(3, 357);
+            this.inputButton.Location = new System.Drawing.Point(3, 367);
             this.inputButton.Name = "inputButton";
-            this.inputButton.Size = new System.Drawing.Size(151, 84);
+            this.inputButton.Size = new System.Drawing.Size(167, 85);
             this.inputButton.TabIndex = 0;
             this.inputButton.Text = "Ввод и вычисление";
             this.inputButton.UseVisualStyleBackColor = true;
@@ -106,9 +106,9 @@ namespace Furry
             // workButton
             // 
             this.workButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.workButton.Location = new System.Drawing.Point(160, 357);
+            this.workButton.Location = new System.Drawing.Point(176, 367);
             this.workButton.Name = "workButton";
-            this.workButton.Size = new System.Drawing.Size(151, 84);
+            this.workButton.Size = new System.Drawing.Size(168, 85);
             this.workButton.TabIndex = 1;
             this.workButton.Text = "Показать график";
             this.workButton.UseVisualStyleBackColor = true;
@@ -122,16 +122,16 @@ namespace Furry
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
             this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox.Size = new System.Drawing.Size(308, 260);
+            this.textBox.Size = new System.Drawing.Size(341, 267);
             this.textBox.TabIndex = 2;
             // 
             // inputTextBox
             // 
             this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inputTextBox.Location = new System.Drawing.Point(3, 269);
+            this.inputTextBox.Location = new System.Drawing.Point(3, 276);
             this.inputTextBox.Multiline = true;
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(151, 82);
+            this.inputTextBox.Size = new System.Drawing.Size(167, 85);
             this.inputTextBox.TabIndex = 3;
             this.inputTextBox.Text = "0";
             // 
@@ -148,20 +148,25 @@ namespace Furry
             this.tableLayoutPanel3.Controls.Add(this.numericUpDown3, 2, 1);
             this.tableLayoutPanel3.Controls.Add(this.numericUpDown2, 1, 1);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(160, 269);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(176, 276);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(151, 82);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(168, 85);
             this.tableLayoutPanel3.TabIndex = 4;
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.numericUpDown1.Location = new System.Drawing.Point(3, 44);
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 45);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
             this.numericUpDown1.TabIndex = 0;
             this.numericUpDown1.Value = new decimal(new int[] {
             5,
@@ -175,7 +180,7 @@ namespace Furry
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 41);
+            this.label1.Size = new System.Drawing.Size(50, 42);
             this.label1.TabIndex = 1;
             this.label1.Text = "interval radius";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -184,9 +189,9 @@ namespace Furry
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(53, 0);
+            this.label2.Location = new System.Drawing.Point(59, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 41);
+            this.label2.Size = new System.Drawing.Size(50, 42);
             this.label2.TabIndex = 2;
             this.label2.Text = "stride";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -195,9 +200,9 @@ namespace Furry
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(103, 0);
+            this.label3.Location = new System.Drawing.Point(115, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 41);
+            this.label3.Size = new System.Drawing.Size(50, 42);
             this.label3.TabIndex = 3;
             this.label3.Text = "member count";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -205,9 +210,14 @@ namespace Furry
             // numericUpDown3
             // 
             this.numericUpDown3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.numericUpDown3.Location = new System.Drawing.Point(103, 44);
+            this.numericUpDown3.Location = new System.Drawing.Point(115, 45);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown3.Size = new System.Drawing.Size(50, 20);
             this.numericUpDown3.TabIndex = 4;
             this.numericUpDown3.Value = new decimal(new int[] {
             5,
@@ -224,9 +234,9 @@ namespace Furry
             0,
             0,
             131072});
-            this.numericUpDown2.Location = new System.Drawing.Point(53, 44);
+            this.numericUpDown2.Location = new System.Drawing.Point(59, 45);
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(50, 20);
             this.numericUpDown2.TabIndex = 5;
             this.numericUpDown2.Value = new decimal(new int[] {
             5,
@@ -236,27 +246,27 @@ namespace Furry
             // 
             // plotChart
             // 
-            chartArea3.Name = "ChartArea1";
-            this.plotChart.ChartAreas.Add(chartArea3);
+            chartArea1.Name = "ChartArea1";
+            this.plotChart.ChartAreas.Add(chartArea1);
             this.plotChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend3.Name = "Legend1";
-            this.plotChart.Legends.Add(legend3);
-            this.plotChart.Location = new System.Drawing.Point(323, 3);
+            legend1.Name = "Legend1";
+            this.plotChart.Legends.Add(legend1);
+            this.plotChart.Location = new System.Drawing.Point(356, 3);
             this.plotChart.Name = "plotChart";
-            series3.ChartArea = "ChartArea1";
-            series3.IsVisibleInLegend = false;
-            series3.Legend = "Legend1";
-            series3.Name = "График функции";
-            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
-            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
-            this.plotChart.Series.Add(series3);
-            this.plotChart.Size = new System.Drawing.Size(474, 444);
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "График функции";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.plotChart.Series.Add(series1);
+            this.plotChart.Size = new System.Drawing.Size(525, 455);
             this.plotChart.TabIndex = 2;
             // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(884, 461);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "MainForm";
